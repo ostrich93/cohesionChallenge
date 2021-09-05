94d9185411318cd79453b554d486990adc703b79
Alex <alexmorson95@gmail.com>
2021-09-05 15:46:19 -0500

    Just realized the CurrentStatus in the request body is a string and that the model object doesn't handle that, so I added it in.

--- cohesionChallenge/Models/ServiceRequest.cs
+++ cohesionChallenge/Models/ServiceRequest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace cohesionChallenge.Models
 {
@@ -12,6 +13,8 @@ namespace cohesionChallenge.Models
         public Guid Id { get; set; }
         public string BuildingCode { get; set; }
         public string Description { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public CurrentStatus CurrentStatus { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
